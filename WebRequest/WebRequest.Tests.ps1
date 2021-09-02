3dc99712dac985881d0e6c2cebda56dc3d86aadd
megafluffles <justin.dean.brown@gmail.com>
2021-09-02 21:09:12 +1000

    Added Mock.

--- WebRequest/WebRequest.Tests.ps1
+++ WebRequest/WebRequest.Tests.ps1
@@ -4,6 +4,7 @@ Describe 'WebRequest' {
     Context 'When response is Hello' {
         It 'should return Hello' {
             # Arrange
+            Mock Invoke-WebRequest { return 'Hello' }
             [WebRequest]$sut = `
                 [WebRequest]::new()
 

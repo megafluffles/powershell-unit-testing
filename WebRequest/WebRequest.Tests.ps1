0da7f909dee7d46226a29ce0020fb69223ec1ca9
megafluffles <justin.dean.brown@gmail.com>
2021-09-02 20:57:34 +1000

    Added WebRequest.

---
+++ WebRequest/WebRequest.Tests.ps1
@@ -0,0 +1,16 @@
+Using Module ./WebRequest.psm1
+
+Describe 'WebRequest' {
+    Context 'When response is Hello' {
+        It 'should return Hello' {
+            # Arrange
+            [WebRequest]$sut = [WebRequest]::new()
+
+            # Act
+            [string]$response = $sut.CallWeb()
+
+            # Assert
+            $response | Should -Be 'Hello'
+        }
+    }
+}
---
+++ WebRequest/WebRequest.psm1
@@ -0,0 +1,10 @@
+class WebRequest {
+    WebRequest() {
+
+    }
+    [string]CallWeb() {
+        [BasicHtmlWebResponseObject]$response = `
+            Invoke-WebRequest -Uri 'https://jsonplaceholder.typicode.com/todos/1'
+        return $response.Content
+    }
+}

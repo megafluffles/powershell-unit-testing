dc37cc0485a1ded693585a32282c92ce296426b7
megafluffles <justin.dean.brown@gmail.com>
2021-09-02 21:22:30 +1000

    Fix.

--- WebRequest/WebRequest.Tests.ps1
+++ WebRequest/WebRequest.Tests.ps1
@@ -1,10 +1,16 @@
 Using Module ./WebRequest.psm1
+Using System.Net.Http
 
 Describe 'WebRequest' {
     Context 'When response is Hello' {
         It 'should return Hello' {
             # Arrange
-            Mock -ModuleName WebRequest Invoke-WebRequest { return 'Hello' }
+            Mock -ModuleName WebRequest Invoke-WebRequest { 
+                [HttpResponseMessage]$responseMessage = [HttpResponseMessage]::new()
+                $responseMessage.Content = 'Hello'
+                [BasicHtmlWebResponseObject]$responseObject = [BasicHtmlWebResponseObject]::new($responseMessage)
+                return $responseObject
+            }
             [WebRequest]$sut = `
                 [WebRequest]::new()
 
